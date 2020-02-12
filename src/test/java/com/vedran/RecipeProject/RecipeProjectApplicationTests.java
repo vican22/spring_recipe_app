bc46fb8eb60a3573093714713d2dd9108976c7e2
Vedran <vedranknezevic1@yahoo.com>
2020-02-12 19:11:51 +0100

    Initial Commit

---
+++ src/test/java/com/vedran/RecipeProject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedran.RecipeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
